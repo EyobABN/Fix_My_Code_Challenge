548fe30edcbb7e95b99715373d1dedc170f66f7b
EyobABN <eyob.alamerew.belay@gmail.com>
2022-10-06 10:20:30 +0300

    Attempt accepting only one side

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,8 +4,7 @@
 
 class Square:
     """Represents a rectangle with equal sides."""
-    width = 0
-    height = 0
+    side = 0
 
     def __init__(self, *args, **kwargs):
         """ Initializes a new square. """
@@ -14,19 +13,19 @@ class Square:
 
     def area_of_my_square(self):
         """ Computes the area of the square. """
-        return self.width * self.height
+        return self.side ^ 2
 
     def perimeter_of_my_square(self):
         """ Computes the perimeter of the square. """
-        return (self.width * 2) + (self.height * 2)
+        return self.side * 4
 
     def __str__(self):
         """ Computes the string format of this square. """
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.side, self.side)
 
 
 if __name__ == "__main__":
-    s = Square(width=12, height=9)
+    s = Square(side=12)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
